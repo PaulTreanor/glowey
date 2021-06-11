f349fcf2b8f5b2dddbb931d4c9f96214c43c3e4a
Paul Treanor <treanorpaul9@gmail.com>
2021-06-11 21:49:04 +0100

    Generating commands from JSON
    Brightness commands now being generated based off config.json

--- glowey.py
+++ glowey.py
@@ -1,8 +1,56 @@
-import subprocess
+#!/usr/bin/python3
+import os
+import json
+
+MIN_VALUE, MAX_VALUE = 0.1, 1
+
+def readJSON():
+    with open('config.json') as configFile:
+        config = json.load(configFile)
+    return config
+
+
+def changeValues(input):
+    # readJSON
+    # increment value 
+    # if values greater than 1 or less than 0
+    #   pass
+    # config['brightness'] = value
+    # config []
+
+    old_values = readJSON()
+
+    if input == "up":
+        old_values['brightness'] += 0.1
+    else:
+        old_values['brightness'] -= 0.1
+
+    if old_value['brightness'] in [0, 1.1]:       # min is 0.1 to avoid making screens unreadble for people
+        return none 
+
+    if input = 1
+
 
 
 def main():
-    subprocess.run(["ls"])
+
+    # Get input ("up", "down")
+
+
+
+    # Change JSON values
+
+
+
+    # Run command 
+
+    config = readJSON()
+    brightness = config['brightness']
+    temperature = config['temperature']
+    output = config['output']
+
+    command = "xrandr --output {} --gamma 1:1:{} --brightness {}".format(output, temperature, brightness)
+    os.system(command)
 
 
 
